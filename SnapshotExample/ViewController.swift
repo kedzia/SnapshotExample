ec6d47ce1103d0ba79b8ab36eabc98a93b705c59
Adam Kędzia <akedzia@egnyte.com>
2019-08-11 16:47:13 +0200

    Initial Commit

---
+++ SnapshotExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapshotExample
+//
+//  Created by MP0091 on 11/08/2019.
+//  Copyright © 2019 Adam Kędzia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
